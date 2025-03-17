d8d8777ff7bd804e76d3fc46b4672d892c982c77
Manish Bhomjan <bhomjanmanish4@gmail.com>
2025-03-17 20:48:54 +0545

    Updated love website with new changes

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ button.addEventListener("click", function(){
 function progressInterva(){
     if(count == 100 && percentWidth == 100){
         clearInterval(progressLoad)
-        percent.textContent = "All set, my love! 😘❤️";
+        percent.textContent = "All set my love!❤️";
         percent.style.letterSpacing = "1px";
         textBox.style.transform = "scale(1.3)"
         heartItem3.style.animation = "1s heartScale forwards"
